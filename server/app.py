419c6882820bd11ed4ed19e2b5ff84cf6b9bbac4
Rafael Finol <rafaelfinol88@gmail.com>
2023-09-13 00:44:39 -0400

    lab complete

--- server/app.py
+++ server/app.py
@@ -6,33 +6,65 @@ from flask_migrate import Migrate
 from models import db, Bakery, BakedGood
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+    bakeries.append(bakery_dict)
 
-@app.route('/bakeries/<int:id>')
+    response = make_response(bakeries, 200)
+
+    return response
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
 
-@app.route('/baked_goods/by_price')
+    bakery_dict = bakery.to_dict()
+
+    response = make_response(bakery_dict, 200)
+
+    return response
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods = []
+    for baked_good in BakedGood.query.order_by(BakedGood.price).all():
+        baked_good_dict = baked_good.to_dict()
+    baked_goods.append(baked_good_dict)
 
-@app.route('/baked_goods/most_expensive')
+    response = make_response(baked_goods, 200)
+
+    return response
+
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
 
-if __name__ == '__main__':
+    baked_good_dict = baked_good.to_dict()
+
+    response = make_response(baked_good_dict, 200)
+
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
